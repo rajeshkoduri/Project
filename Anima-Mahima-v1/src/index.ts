41a766816564a2465fd7975fbf98ab54981acc33
Rajesh Koduri <rajeshkoduri29@outlook.com>
2022-07-23 08:55:14 +0530

    Server able to log filename, width and height

--- Anima-Mahima-v1/src/index.ts
+++ Anima-Mahima-v1/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello Workld!!!');
+import express from 'express'
+import resizeImages from './api/images/images';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, ()=> {
+    console.log('server started...');
+    
+})
+//resize images endpoint
+app.get('/api/images', resizeImages, (req,res)=>{
+    res.send(req.query);
+} )
