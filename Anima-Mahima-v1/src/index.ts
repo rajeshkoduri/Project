09fb5bd27eb7006dcf5492e3285a4fd70bef976e
Rajesh Koduri <rajeshkoduri29@outlook.com>
2022-07-23 19:29:10 +0530

    ran once to get thumbs folder and output of the run

--- Anima-Mahima-v1/src/index.ts
+++ Anima-Mahima-v1/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import resizeImages from './api/images/resizeImg';
 
 const app = express();
-const port = 3000;
+const port = 3005;
 
 app.listen(port, ()=> {
     console.log('server started...');
